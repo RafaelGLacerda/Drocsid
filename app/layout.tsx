02030c9a153f2d318967e5fcd94d9df281f1b87f
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2025-06-04 18:33:09 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Drocsid',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
 
 export default function RootLayout({
-  children,
+  children, 
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/icone.png" type="image/png" />
       <body>{children}</body>
     </html>
   )
